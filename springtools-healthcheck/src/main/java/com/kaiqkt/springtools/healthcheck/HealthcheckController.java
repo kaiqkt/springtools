eacb61a1987aea49190a3b83b83f9d2a5a6eba9f
kaiqkt <kaiquebeserra1313@gmail.com>
2024-04-24 17:06:38 -0300

    autowired build properties

--- springtools-healthcheck/src/main/java/com/kaiqkt/springtools/healthcheck/HealthcheckController.java
+++ springtools-healthcheck/src/main/java/com/kaiqkt/springtools/healthcheck/HealthcheckController.java
@@ -1,5 +1,6 @@
 package com.kaiqkt.springtools.healthcheck;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,12 @@ import java.util.Map;
 @RestController
 public class HealthcheckController {
 
-    private BuildProperties buildProperties;
+    private final BuildProperties buildProperties;
+
+    @Autowired
+    public HealthcheckController(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
 
     @GetMapping("/healthcheck")
     public ResponseEntity<Map<String, Object>> check(){
